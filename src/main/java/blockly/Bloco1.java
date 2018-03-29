07b8f14787e2420415624d58108a2e2c727f7099
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-03-29 14:30:10 +0000

    First commit...

---
+++ src/main/java/blockly/Bloco1.java
@@ -0,0 +1,27 @@
+package blockly;
+
+import cronapi.*;
+import cronapi.rest.security.CronappSecurity;
+import java.util.concurrent.Callable;
+
+@CronapiMetaData(type = "blockly")
+@CronappSecurity
+public class Bloco1 {
+
+	public static final int TIMEOUT = 300;
+
+	/**
+	 *
+	 * @return Var
+	 */
+	// Bloco1
+	public static Var Executar() throws Exception {
+		return new Callable<Var>() {
+
+			public Var call() throws Exception {
+				return Var.VAR_NULL;
+			}
+		}.call();
+	}
+
+}
